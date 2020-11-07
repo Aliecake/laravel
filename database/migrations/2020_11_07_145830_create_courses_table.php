101b27962d0264b3e947d23858c2b28726216760
Lauren Eggers <neyabenz@gmail.com>
2020-11-07 10:05:24 -0500

    database setup for courses

---
+++ database/migrations/2020_11_07_145830_create_courses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->longText('summary');
+            $table->integer('duration');
+            $table->string('language');
+            $tab;e->string('difficulty');
+            $tab;e->integer('stages');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
